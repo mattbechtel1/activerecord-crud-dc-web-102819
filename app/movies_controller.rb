60d2d5bc82e18a959efb37d85ff86f2fafe64639
Matt Bechtel <bechma02@gmail.com>
2019-11-10 04:32:40 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -48,11 +48,11 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find_by(id: 1)
+  Movie.find_by id: 1
 end
 
 def can_find_by_multiple_attributes
-  Movie.find_by(attrbute1, attribute2=nil, attribute3=nil)
+  Movie.find_by_sql()
   # Search Values:
   # title == "Title"
   # release_date == 2000
